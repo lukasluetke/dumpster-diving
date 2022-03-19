66e55f46117ab6dda8065d397a77054189c6bc44
wollix <wollix@jwebserver.com>
2022-03-19 20:57:37 +0100

    Show Marker name in Popup
    Signed-off-by: wollix <wollix@jwebserver.com>

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -18,13 +18,13 @@
         :options="{zoomControl: false}">
         <l-tile-layer style="z-index: 100" url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"></l-tile-layer>
           <l-marker 
-            @click="openModal(latLng)"
+            @click="openModal()"
             v-for="(marker, index) in markers"
             :key="index"
             ref="markersRef"
-            :lat-lng="marker"
+            :lat-lng="marker.coord"
             >
-            <l-popup :content="marker.toString()"></l-popup>
+            <l-popup :content="marker.name"></l-popup>
           </l-marker>
       </l-map>
     </ion-content>
@@ -36,7 +36,7 @@ import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import "leaflet/dist/leaflet.css"
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
-import { supabase } from "../supabase"
+import { supabase } from "@/supabase"
 import { modalController } from '@ionic/vue';
 import Modal from './SpotModal.vue'
 
@@ -71,13 +71,13 @@ export default defineComponent({
   }),
   methods: {
     populate_markers_array: function (marker_objects) {
-      let markersArr = []
-      marker_objects.forEach(element => {
-        console.log(element.name)
-        let coordsArr = element.coordinates.split(',')
-        markersArr.push([parseFloat(coordsArr[0]), parseFloat(coordsArr[1])])
-      });
-      this.markers = markersArr
+      console.log(marker_objects)
+      this.markers = marker_objects.map(m => {
+        return {
+          name: m.name,
+          coord: m.coordinates.split(', ').map(c => parseFloat(c))
+        }
+      })
     },
     async openModal() {
       const modal = await modalController
