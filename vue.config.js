7701479776b016217fa9dbeb7535f904e3281922
lukasluetke <lukasluetke3@gmail.com>
2022-04-21 17:34:13 +0200

    favicons

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js file to be placed in the root of your repository
+
+module.exports = {
+    pwa: {
+        workboxOptions: {
+          exclude: [/_redirects/]
+        }
+      }
+  }
+  
